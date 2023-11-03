840d11d03597fc4004f0026c4f1c4e1a8e89a777
twardoch <adam@twardoch.com>
2023-11-03 17:04:47 +0100

    up

--- scripts/fontlab_export.py
+++ scripts/fontlab_export.py
@@ -12,7 +12,7 @@ def pprint(msg):
 class FontLabFontExport:
     def __init__(self):
         self.qApp = QGuiApplication.instance()
-        self.script_folder = Path(__file__).resolve().parent
+        #self.script_folder = Path(__file__).resolve().parent
         self.args = self.qApp.arguments()
         self.fl_workspace = flWorkspace.instance()
         self.fl_main = self.fl_workspace.mainWindow
@@ -26,11 +26,12 @@ class FontLabFontExport:
     def openFont(self, input_path):
         self.input_path = Path(input_path).resolve()
         self.font_base_name = self.input_path.stem
-        self.fl_items.notifyWorkspaceInitialized(self.fl_workspace)
-        self.fl_items.init()
+        #self.fl_items.notifyWorkspaceInitialized(self.fl_workspace)
+        #self.fl_items.init()
         self.fl_main.loadFont(str(self.input_path))
-        pprint(f">> Loaded: {self.input_path}")
         self.fl_package = self.fl_workspace.currentPackage
+        self.fl_workspace.addPackage(self.fl_package)
+        pprint(f">> Opened: {self.input_path}")
 
     def setBoolPref(self, pref_key, val):
         fl_prefs = flPreferences()
@@ -58,29 +59,34 @@ class FontLabFontExport:
         fl_export.groupProfiles = False
         fl_export.groupFamily = False
 
-        pprint(f">> Exporting to {self.output_folder}")
         self.fl_workspace.exportFont(self.fl_package, fl_export)
+        pprint(f">> Exported: {self.output_folder}")
 
-    def closePackages(self):
-        for pi in range(len(self.fl_workspace.packages), -1, -1):
-            self.fl_workspace.closePackage(packages[pi])
+    def closeAll(self):
+        packages = list(self.fl_workspace.packages())
+        pprint(f">> Closing {len(packages)} fonts")
+        for packobj in packages:
+            self.fl_workspace.currentPackage.close(False)
 
     def convert(self, input_path, output_folder, profile_name="OpenType TT"):
+        pprint(f">> Converting {input_path} to {output_folder} as {profile_name}")
         self.openFont(input_path)
         self.exportFont(output_folder, profile_name)
 
     def quit(self):
-        self.closePackages()
-        self.qApp.processEvents()
+        pprint(f">> Quitting FontLab")
         self.runQAction("mainwindow.actionExit")
 
+    def finish(self):
+        QTimer.singleShot(100, self.closeAll)
+        self.qApp.processEvents()
+        QTimer.singleShot(100, self.quit)
+        
 
 def convert(input_path, output_folder, profile_name="OpenType TT"):
     fl_font_export = FontLabFontExport()
-    fl_font_export = convert(input_path, output_folder, profile_name)
-    pprint(f">> Quitting")
-    QTimer.singleShot(200, fl_font_export.quit)
-
+    fl_font_export.convert(input_path, output_folder, profile_name)
+    fl_font_export.finish()
 
 if __name__ == "__main__":
     fl_main()
